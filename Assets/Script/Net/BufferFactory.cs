7d4d54ca48f06ef465c9fcb96e457e5c2d0592c3
shuitiangong <757153944@qq.com>
2023-02-11 17:50:56 +0800

    导入protobuf

---
+++ Assets/Script/Net/BufferFactory.cs
@@ -0,0 +1,28 @@
+﻿using Google.Protobuf;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Net
+{
+    public class BufferFactory
+    {
+        enum MessageType
+        {
+            ACK = 0, //确认报文
+            Logic = 1, //业务逻辑的报文
+        }
+        /// <summary>
+        /// 创建并且发送报文
+        /// </summary>
+        /// <param name="messageID"></param>
+        /// <param name="message"></param>
+        public static void CreateAndSendPackage(int messageID, IMessage message)
+        {
+            BufferEntity buffer = new BufferEntity(USocket.local.endPoint, USocket.local.sessionID,
+                0, 0, MessageType.Logic.GetHashCode(), messageID, ProtobufHelper.ToBytes(message));
+            USocket.local.Send(buffer);
+        }
+    }
+}
+
