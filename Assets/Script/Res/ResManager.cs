dbe34273214132032b3ddc28fb26e8ee5aabb1a8
shuitiangong <757153944@qq.com>
2023-02-13 13:40:56 +0800

    资源加载管理

---
+++ Assets/Script/Res/ResManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResManager : Singleton<ResManager>
+{
+    //加载UI窗体
+    public GameObject LoadUI(string path)
+    {
+        GameObject res = Resources.Load<GameObject>($"UIPrefab/{path}");
+        if (res==null)
+        {
+            Debug.LogError($"没有找到UI窗体：{path}");
+            return null;
+        }
+        GameObject obj = GameObject.Instantiate(res);
+        return obj;
+    }
+}
