9ded35d8645b6c31d20aa6d56e497ea347b1f71f
shuitiangong <757153944@qq.com>
2023-02-18 22:09:53 +0800

    状态机

---
+++ Assets/Script/FSM/EntityFSM.cs
@@ -0,0 +1,42 @@
+﻿using ProtoMsg;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityFSM 
+{
+    public Transform transform;
+    public PlayerFSM fsm; //管理类
+
+    //进入状态
+    public virtual void Enter() {
+        AddListener();
+    }
+
+    //状态更新中
+    public virtual void Update() { 
+    
+    }
+
+    //退出状态
+    public virtual void Exit() {
+        RemoveListener();
+    }
+
+    //监听一些游戏事件
+    public virtual void AddListener() { 
+    
+    }
+
+    //移除掉监听的事件
+    public virtual void RemoveListener()
+    {
+
+    }
+
+    //处理每一帧的网络事件
+    public virtual void HandleCMD(BattleUserInputS2C s2cMSG)
+    {
+
+    }
+}
---
+++ Assets/Script/FSM/FSMState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FSMState 
+{
+   None,//空
+   Idle,//休闲
+   Move,//跑
+   Skill,//技能中
+   Dead,//死亡
+   Relive,//复活
+}
